773c3b4a9bf0d0fb1ba4363b6b2cb9cc83c6a1ed
katie-y <katie_york@hotmail.co.uk>
2020-02-21 16:38:13 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+class Country {
+    constructor(name, population){
+        this.name = name;
+        this.population = population;
+    }
+}
+
+let china = new Country("China", 1401.43);
+let england = new Country("England", 55.98);
+let india = new Country("India", 1358.90);
+let unitedStates = new Country("United States", 329.32);
+
+
+console.log(china);
